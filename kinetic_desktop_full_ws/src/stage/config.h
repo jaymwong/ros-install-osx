2b4546080ef34738e046c71e43dfd43e4cbef181
Jay M. Wong <jay@southieautonomyworks.com>
2020-06-09 08:54:04 -0400

    Updating several modules

---
+++ kinetic_desktop_full_ws/src/stage/config.h
@@ -0,0 +1,13 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define PROJECT "Stage"
+#define VERSION "4.1.1"
+#define APIVERSION "4.1"
+#define INSTALL_PREFIX "/opt/ros/kinetic"
+#define PLUGIN_PATH "/opt/ros/kinetic/lib/Stage-4.1"
+
+/* #undef BUILD_GUI */
+
+#endif
+
